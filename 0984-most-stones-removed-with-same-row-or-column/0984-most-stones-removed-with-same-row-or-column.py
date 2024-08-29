0b143566ab847ce574afa54873462abdc1aa31d1
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2024-08-30 00:03:31 +0800

    Time: 115 ms (94.55%), Space: 17.2 MB (71.81%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -1,52 +1,56 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         # group all stones connected to each other together
+        n = len(stones)
+        rank = [1 for i in range(n)]
+        parent = [i for i in range(n)]
         mapRow = {}
         mapCol = {}
-        N = len(stones)
-        for i in range(N):
-            stone = stones[i]
+        for index, stone in enumerate(stones):
             r, c = stone[0], stone[1]
             if r not in mapRow:
-                mapRow[r] = [i]
+                mapRow[r] = [index]
             else:
-                mapRow[r].append(i)
+                mapRow[r].append(index)
+            
             if c not in mapCol:
-                mapCol[c] = [i]
+                mapCol[c] = [index]
             else:
-                mapCol[c].append(i)
-        
-        parent = [i for i in range(N)]
-        rank = [1 for i in range(N)]
+                mapCol[c].append(index)
+
         def find(i):
-            while i != parent[i]:
+          
+            while parent[i] != i:
                 parent[i] = parent[parent[i]]
                 i = parent[i]
             return i
-        def union(a, b):
-            pA, pB = find(a), find(b)
-            if pA == pB:
+        
+        def union(i, j):
+            p1, p2 = find(i), find(j)
+            if p1 == p2:
                 return
-            if rank[pA] > rank[pB]:
-                rank[pA] += rank[pB]
-                parent[pB] = pA
+            if rank[p1] > rank[p2]:
+                rank[p1] += rank[p2]
+                parent[p2] = p1
             else:
-                rank[pB] += rank[pA]
-                parent[pA] = pB
+                rank[p2] += rank[p1]
+                parent[p1] = p2
+        
         for group in mapRow.values():
-            start = group[0]
-            for i in range(1, len(group)):
-                union(start, group[i])
+            if len(group) > 1:
+                first = group[0]
+                for j in group[1:]:
+                    union(first, j)
+        
         for group in mapCol.values():
-            start = group[0]
-            for i in range(1, len(group)):
-                union(start, group[i])
+            if len(group) > 1:
+                first = group[0]
+                for j in group[1:]:
+                    union(first, j)
+        
         hashSet = set()
-        for i in range(N):
-            parent[i] = find(i) 
+        for i in range(n):
+            parent[i] = find(i)
             hashSet.add(parent[i])
-        return N - len(hashSet)   
-
-        # find number of connected components
-
-        # return numStones - numComponents
+        
+        return len(stones) - len(hashSet)
