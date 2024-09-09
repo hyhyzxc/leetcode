05164a5dfca62e6e46c2ec8e849cd09afb83e1e2
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2024-09-09 21:27:45 +0800

    Time: 745 ms (21.3%), Space: 56.4 MB (34.49%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.py
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.py
@@ -9,12 +9,12 @@ class Solution:
         curr = head
         res = [[-1 for i in range(n)] for j in range(m)]
         pos = (0,0)
-        visited = set()
         i = 0
         while curr:
-            x, y = direction[i%4]
+            dirIndex = i % 4
+            x, y = direction[dirIndex]
             r, c = pos
-            print(r, c, dir)
+            #print(r, c, dir)
             while r >= 0 and r < m and c >= 0 and c < n and res[r][c] == -1:
                 res[r][c] = curr.val
                 curr = curr.next
@@ -26,6 +26,7 @@ class Solution:
             
             pos = (r-x, c-y)
             i += 1
-            pos = (pos[0] + direction[i%4][0], pos[1] + direction[i%4][1])
+            dirIndex = i % 4
+            pos = (pos[0] + direction[dirIndex][0], pos[1] + direction[dirIndex][1])
                 
         return res
