cd95facd7b22e1bba795800b6268fad0d64b2f18
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2024-09-17 00:06:15 +0800

    Time: 70 ms (58.82%), Space: 19.4 MB (97.94%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -3,10 +3,9 @@ class Solution:
         nums = []
         for time in timePoints:
             nums.append(int(time[:2]) * 60 + int(time[3:]))
-            if int(time[:2]) < 12:
-                nums.append(nums[-1] + 24 * 60)
+       
         nums.sort()
-      
+        nums.append(nums[0] + 24*60)
         res = 10 ** 5
         for i in range(1, len(nums)):
             res = min(res, min(nums[i] - nums[i-1], nums[i-1] + 24 * 60 - nums[i]))
