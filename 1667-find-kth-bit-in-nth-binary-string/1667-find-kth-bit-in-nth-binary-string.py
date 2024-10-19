3dbc86fc2fd0b16ae26fb6a2f11f6935790b65fa
hyhyzxc <88191396+hyhyzxc@users.noreply.github.com>
2024-10-19 21:23:52 +0800

    Time: 62 ms (49.51%), Space: 20.2 MB (43.79%) - LeetHub

---
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        dp = {1: '0'}
+        for i in range(2, n+1):
+            x = bin(int(dp[i-1], 2) ^ int("1" * len(dp[i-1]), 2))[2:][::-1]
+            dp[i] = dp[i-1] + "1" + str(x)
+            print(x)
+        return dp[n][k-1]
